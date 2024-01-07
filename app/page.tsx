8c6a4eb0ce02023cbfca7267092e72380c80adde
VaishakhMenon <133619631+VaishakhMenon@users.noreply.github.com>
2024-01-07 12:38:58 +0530

    Added para

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
+    <>
     <h1>Hello Spoti</h1>
+      <p>Spoti is an application that shows nearby events and businesses offering discounts.</p>
+      </>
   )
 }
